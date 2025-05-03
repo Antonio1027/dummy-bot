89116ce75103f29ab215ae338bce71be22f4d912
Antonio Shilon <antonio@Antonios-MacBook-Pro.local>
2025-05-03 01:43:25 -0600

    feat: openai api call

--- src/app/services/openia/api.py
+++ src/app/services/openia/api.py
@@ -1,16 +1,16 @@
 import os
-import openai
+from openai import OpenAI
 
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())
 
-openai.api_key = os.environ['OPENAI_API_KEY']
+client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
 
 def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0, max_tokens=500):
-    response = openai.ChatCompletion.create(
+    response = client.chat.completions.create(
         model=model,
         messages=messages,
         temperature=temperature,
         max_tokens=max_tokens,
     )
-    return response.choises[0].message["content"]
+    return response.choices[0].message.content
